46adb93b60665ed3090ac29ab30259eb036ae2a6
Ayman-ss <ayman.sohail@ontariotechu.net>
2025-11-25 15:42:58 -0500

    added requireSignIN in workout.js so Users are required to sign in to access planner

--- routes/signin.js
+++ routes/signin.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/users.models");
+const User = require("../models/users.models.js");
 
 // GET /signin – show the sign in form
 router.get("/signin", (req, res) => {
